814f7e174a5b46b3fc63b5fb82b5ae792bf6d9a5
RachelKoldenhoven <rachel@koldenfrozen.com>
2016-03-15 09:31:21 -0600

    added routes to index.js for index, register, login, cultures, cultures:id, public/user/id

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -9,4 +9,24 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/register', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/login', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/cultures', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/cultures/:id', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/public/user/:id', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
 module.exports = router;
