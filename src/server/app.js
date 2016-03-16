956a35192917945932eb00a381540b9b9d08a0e3
RachelKoldenhoven <rachel@koldenfrozen.com>
2016-03-16 10:23:27 -0600

    preparing for heroku

--- src/server/app.js
+++ src/server/app.js
@@ -125,8 +125,9 @@ app.use(function(err, req, res, next) {
   });
 });
 
-app.listen(3001, function() {
-  console.log('Express app listening on port 3001');
+var port = process.env.PORT || 3001;
+app.listen(port, function() {
+  console.log('Express app listening on port ' + port);
 });
 
 
