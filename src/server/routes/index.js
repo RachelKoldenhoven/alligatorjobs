a56d968eed13b9ab40c7b89e1f8221c731fc5493
RachelKoldenhoven <rachel@koldenfrozen.com>
2016-03-16 11:00:20 -0600

    building out routes for register and local auth

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -8,8 +8,46 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Alligator Jobs', user: req.user? req.user.fname: "" });
 });
 
-router.get('/register', function(req, res, next) {
-  res.render('register', { title: 'Alligator Job', user: req.user? req.user.fname: "" });
+router.get('/register', helpers.loginRedirect, function(req, res, next) {
+  res.render('register', { title: 'Alligator Job', user: req.user});
+});
+
+router.post('/register', function(req, res, next) {
+  var email = req.body.email;
+  var password = req.body.password;
+  // check if email is unique
+  knex('users').where('email', email)
+    .then(function(data){
+      // if email is in the database send an error
+      if(data.length) {
+        req.flash('message', {
+          status: 'danger',
+          message: 'Email already exists.!'
+        });
+        return res.redirect('/register');
+      } else {
+        // hash and salt the password
+        var hashedPassword = helpers.hashing(password);
+        // if email is not in the database insert it
+        knex('users').insert({
+            email: email,
+            password: hashedPassword
+          })
+          .then(function(data) {
+            req.flash('message', {
+              status: 'success',
+              message: 'Welcome!'
+            });
+            return res.redirect('/login');
+          })
+          .catch(function(err) {
+            return res.send('crap');
+          });
+      }
+    })
+    .catch(function(err){
+      return next(err);
+    });
 });
 
 router.get('/login', function(req, res, next) {
