48b7a5555302b5c64411b4ee42c3ec9c83c25eb4
Daniel Klein <danhirschklein@gmail.com>
2016-03-16 11:48:55 -0600

    Created Culture queries and route

--- queries2.js
+++ queries2.js
@@ -30,6 +30,14 @@ module.exports = {
                         +' where we.user_id ='+id);
     },
 
+    getSingleCulture: function(id) {
+        return knex('cultures').where('id', id);
+    },
+
+    getCultureResources: function(culture_id) {
+        return knex('resources').where('culture_id', culture_id);
+    },
+
     registerUser: function(newUser) {
         return knex.insert({
             google_id: newUser.google_id,
