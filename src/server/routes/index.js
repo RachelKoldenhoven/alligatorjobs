fa7596b8712c9d5db25d90e5e0f607d913b8a721
RachelKoldenhoven <rachel@koldenfrozen.com>
2016-03-24 20:10:24 -0600

    problem solving contact us bug

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -181,8 +181,8 @@ router.post('/contact', function(req, res, next) {
   };
   transporter.sendMail(mailOptions, function(error, info){
     if(error){
-      //console.log(error);
-      res.json({yo: 'error'});
+      console.log(error);
+      res.json({yo: error});
     }else{
       //console.log('Message sent: ' + info.response);
       res.redirect('/');
